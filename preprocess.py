80d16dab451e70c5ec0693ab43eec688b73a8b5d
Tien Nguyen <monkiradgbn01@gmail.com>
2020-11-30 16:11:19 +0700

    Upload preprocess

---
+++ preprocess.py
@@ -0,0 +1,43 @@
+import numpy as np
+from keras.utils import to_categorical
+
+
+def bbox2_3D(img):
+
+    r = np.any(img, axis=(1, 2))
+    c = np.any(img, axis=(0, 2))
+    z = np.any(img, axis=(0, 1))
+
+    rmin, rmax = np.where(r)[0][[0, -1]]
+    cmin, cmax = np.where(c)[0][[0, -1]]
+    zmin, zmax = np.where(z)[0][[0, -1]]
+
+    return [rmin, rmax, cmin, cmax, zmin, zmax]
+
+
+def normalize_2D_image(img):
+    v_max = img.max(axis=(0, 1), keepdims=True)
+    v_max = np.where(np.abs(v_max) <= 0.000001, 1., v_max)
+
+    return (img) / (v_max)
+
+
+def normalize_3D_image(img):
+    for z in range(img.shape[0]):
+        img[z] = normalize_2D_image(img[z])
+    return img
+
+
+def label_num2cat(label,num_class):
+  new_mask = np.zeros(label.shape + (num_class,))
+  mask = to_categorical(label,5)
+  for i, c in enumerate([0,1,2,4]):
+    new_mask[:,:,:,i] = mask[:,:,:,c]
+  return new_mask
+
+def preprocess(img, mask):
+    img = normalize_3D_image(img)
+    print(img.shape)
+    new_mask = label_num2cat(mask)
+    rmin, rmax, cmin, cmax, zmin, zmax = bbox2_3D(img)
+    return img[:,cmin:cmax], new_mask[cmin:cmax]
